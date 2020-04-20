fe4b23b30d6cb87fac7c152bc2d74350ebc9c420
LzGustavo <xavier.luizgustavo@gmail.com>
2020-04-20 12:32:31 -0300

    implementing linear bubbler reducer

--- gen_test.go
+++ gen_test.go
@@ -26,3 +26,47 @@ func TestInit(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestReduceAlgos(t *testing.T) {
+
+	testCases := []struct {
+		name     string
+		nCmds    int
+		pWrts    int
+		diffKeys int
+		alg      Reducer
+	}{
+		{
+			"Case1",
+			20,
+			100,
+			5,
+			Bubbler,
+		},
+		{
+			"Case2",
+			20000000,
+			90,
+			10000,
+			Bubbler,
+		},
+	}
+
+	for _, tc := range testCases {
+		lg := &ListGenerator{}
+		lg.Seed()
+		l, err := lg.Gen(tc.nCmds, tc.pWrts, tc.diffKeys)
+		if err != nil {
+			t.Log("test", tc.name, "failed with err:", err.Error())
+			t.FailNow()
+		}
+		t.Log("Init:\n", l.Str())
+
+		err = ApplyReduceAlgo(l, tc.alg)
+		if err != nil {
+			t.Log("test", tc.name, "failed with err:", err.Error())
+			t.FailNow()
+		}
+		t.Log("After Reduce:\n", l.Str())
+	}
+}
--- reduce.go
+++ reduce.go
@@ -11,31 +11,81 @@ const (
 	// Bubbler ...
 	Bubbler Reducer = iota
 
+	// Merge ...
+	Merge
+
 	// Greedy ...
 	Greedy
 )
 
 // ApplyReduceAlgo ...
 func ApplyReduceAlgo(s Structure, r Reducer) error {
-	switch r {
-	case Bubbler:
-		BubblerList(s)
-		break
+	switch st := s.(type) {
+	case *List:
+		switch r {
+		case Bubbler:
+			BubblerList(st)
+			break
 
-	case Greedy:
-		GreedyList(s)
+		case Merge:
+			MergeList(st)
+			break
+
+		case Greedy:
+			GreedyList(st)
+			break
+
+		default:
+			return errors.New("unsupported reduce algorithm")
+		}
 		break
 
 	default:
-		return errors.New("unsupported reduce algorithm")
+		return errors.New("unsupported log datastructure")
 	}
 	return nil
 }
 
 // BubblerList is the simpliest algorithm.
-func BubblerList(s Structure) {
+// Remember that the list is represented on the oposite order. The first
+// element is the last proposed index in the command log, its neighbor
+// is the prior-indexed command, and so on.
+func BubblerList(l *List) {
+	var (
+		rm bool
+		rc int
+		i  *listNode
+	)
+
+	for {
+		rm = false
+		for i = l.first; i.next != nil; i = i.next {
+
+			cmd := i.val.(KVCommand)
+			neigh := i.next.val.(KVCommand)
+
+			// Subsequent write operatios over the same key
+			if cmd.op == Write && neigh.op == Write && cmd.key == neigh.key {
+				i.next = i.next.next
+				rm = true
+				rc++
+			}
+
+			// TODO: remove read operations
+		}
+		l.tail = i
+
+		if !rm {
+			break
+		}
+	}
+	l.len -= rc
+}
+
+// MergeList is based on MergeSort algorithm ...
+func MergeList(s *List) {
 }
 
 // GreedyList returns an optimal solution...
-func GreedyList(s Structure) {
+func GreedyList(s *List) {
 }
