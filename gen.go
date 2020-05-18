3d9ad96520f3ffe90f18caa6c39cc2dd23af9fc0
LzGustavo <xavier.luizgustavo@gmail.com>
2020-05-18 11:24:23 -0300

    generating random avl tree struct, source: geeks4geeks

--- gen.go
+++ gen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"math/rand"
 	"time"
 )
@@ -102,5 +103,49 @@ func ListGen(n, wrt, dif int) (Structure, error) {
 
 // AVLTreeGen ...
 func AVLTreeGen(n, wrt, dif int) (Structure, error) {
-	return nil, nil
+
+	srand := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(srand)
+
+	ht := make(stateTable, 0)
+	avl := &AVLTreeHT{
+		aux: &ht,
+		len: 0,
+	}
+
+	for i := 0; i < n; i++ {
+
+		// only WRITE nodes are recorded on the tree
+		if cn := r.Intn(100); cn < wrt {
+
+			rkey := r.Intn(dif)
+			cmd := KVCommand{
+				key:   rkey,
+				value: r.Uint32(),
+				op:    Write,
+			}
+
+			aNode := &avlTreeNode{
+				ind: i,
+			}
+
+			// A write cmd always references a new state on the aux hash table
+			st := &State{
+				ind: i,
+				cmd: cmd,
+			}
+
+			// Add state to the list of updates in that particular key
+			lNode := (*avl.aux)[rkey].push(st)
+			aNode.ptr = lNode
+			ok := avl.insert(aNode)
+			if !ok {
+				return nil, errors.New("cannot insert equal keys on BSTs")
+			}
+
+		} else {
+			continue
+		}
+	}
+	return avl, nil
 }
--- structure.go
+++ structure.go
@@ -3,14 +3,12 @@ package main
 import (
 	"fmt"
 	"strings"
-	"sync"
 )
 
 // Structure ...
 type Structure interface {
 	Str() string
 	Len() int
-	Hash()
 }
 
 type listNode struct {
@@ -23,14 +21,10 @@ type List struct {
 	first *listNode
 	tail  *listNode
 	len   int
-	mu    sync.Mutex
 }
 
 // Str returns a string representation of the list state, used for debug purposes.
 func (l *List) Str() string {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
 	var strs []string
 	for i := l.first; i != nil; i = i.next {
 		strs = append(strs, fmt.Sprintf("%v->", i.val))
@@ -43,8 +37,159 @@ func (l *List) Len() int {
 	return l.len
 }
 
-// Hash tranverses the list returning a hash codification for the entire structure.
-func (l *List) Hash() {
-	l.mu.Lock()
-	defer l.mu.Unlock()
+// Push ...
+func (l *List) push(v interface{}) *listNode {
+	nd := &listNode{
+		val: v,
+	}
+	l.tail.next = nd
+	l.tail = nd
+	l.len++
+	return nd
+}
+
+// State represents a ...
+type State struct {
+	ind int
+	cmd KVCommand
+}
+
+// stateTable stores ...
+type stateTable map[int]*List
+
+type avlTreeNode struct {
+	ind int
+	key int
+	ptr *listNode
+
+	left   *avlTreeNode
+	right  *avlTreeNode
+	height int
+}
+
+// AVLTreeHT ...
+type AVLTreeHT struct {
+	root *avlTreeNode
+	aux  *stateTable
+	len  int
+}
+
+// Str ...
+func (av *AVLTreeHT) Str() string {
+	return ""
+}
+
+// Len ...
+func (av *AVLTreeHT) Len() int {
+	return av.len
+}
+
+func (av *AVLTreeHT) rightRotate(root *avlTreeNode) *avlTreeNode {
+	son := root.left
+	gson := son.right
+
+	// rotation
+	son.right = root
+	root.left = gson
+
+	// update heights
+	root.height = max(getHeight(root.left), getHeight(root.right)) + 1
+	son.height = max(getHeight(son.left), getHeight(son.right)) + 1
+	return son
+}
+
+func (av *AVLTreeHT) leftRotate(root *avlTreeNode) *avlTreeNode {
+	son := root.right
+	gson := son.left
+
+	// rotation
+	son.left = root
+	root.right = gson
+
+	// update heights
+	root.height = max(getHeight(root.left), getHeight(root.right)) + 1
+	son.height = max(getHeight(son.left), getHeight(son.right)) + 1
+	return son
+}
+
+func (av *AVLTreeHT) recurInsert(root, node *avlTreeNode) *avlTreeNode {
+
+	if root == nil {
+		return node
+	}
+
+	if node.ind < root.ind {
+		root.left = av.recurInsert(root.left, node)
+
+	} else if node.ind > root.ind {
+		root.right = av.recurInsert(root.right, node)
+
+	} else {
+		// Equal keys are not allowed in BST
+		return nil
+	}
+
+	root.height = 1 + max(getHeight(root.left), getHeight(root.right))
+
+	// If this node becomes unbalanced, then there are 4 cases
+	balance := getBalanceFactor(root)
+
+	// Left Left Case
+	if balance > 1 && node.ind < root.left.ind {
+		return av.rightRotate(root)
+	}
+
+	// Right Right Case
+	if balance < -1 && node.ind > root.right.ind {
+		return av.leftRotate(root)
+	}
+
+	// Left Right Case
+	if balance > 1 && node.ind > root.left.ind {
+		root.left = av.leftRotate(root.left)
+		return av.rightRotate(root)
+	}
+
+	// Right Left Case
+	if balance < -1 && node.ind < root.right.ind {
+		root.right = av.rightRotate(root.right)
+		return av.leftRotate(root)
+	}
+	return root
+}
+
+func (av *AVLTreeHT) insert(node *avlTreeNode) bool {
+	if av.root == nil {
+		node.height = 1
+		av.len++
+		av.root = node
+		return true
+	}
+
+	ok := av.recurInsert(av.root, node) != nil
+	if ok {
+		av.len++
+	}
+	return ok
+}
+
+func getHeight(node *avlTreeNode) int {
+	if node == nil {
+		return 0
+	}
+	return node.height
+}
+
+func getBalanceFactor(node *avlTreeNode) int {
+	if node == nil {
+		return 0
+	}
+	return getHeight(node.left) - getHeight(node.right)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
